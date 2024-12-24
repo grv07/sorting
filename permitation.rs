4cb2e9659775d04d960a68876ef21624da5b31be
Gaurav Tyagi <grvtyagi22@gmail.com>
2024-12-24 09:38:06 +0530

    Permutation wt=ithout using the mark array

--- permitation.rs
+++ permitation.rs
@@ -15,8 +15,28 @@ fn perm(input: &[usize], mut res: Vec<usize>, mark: &mut Vec<usize>) {
     }
 }
 
+fn perm2(input: &mut [usize], i: usize) {
+    if i == input.len() {
+        println!("{input:?}");
+        return;
+    }
+
+    for k in i..input.len() {
+        let temp = input[k];
+        input[k] = input[i];
+        input[i] = temp;
+
+        perm2(input, i + 1);
+
+        let temp = input[i];
+        input[i] = input[k];
+        input[k] = temp;
+    }
+}
+
 fn main() {
     let res = vec![];
     let mut mark = vec![0; 5];
     perm(&[1, 2, 3, 5, 9], res, &mut mark);
+    perm2(&mut [1, 2, 3], 0);
 }
