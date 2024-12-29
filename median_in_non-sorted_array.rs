6138a5a956e2dc22e10656ca9358ed4433b69913
Gaurav Tyagi <grvtyagi22@gmail.com>
2024-12-29 16:16:31 +0530

    sort only k elements in an un ordered array

--- median_in_non-sorted_array.rs
+++ median_in_non-sorted_array.rs
@@ -1,13 +1,13 @@
 fn main() {
-    let mut input = vec![12, 0, 4, 1, 2, 4, 5, 90, 67, 0, 15, 11, 10, 17];
-    // let mut input = vec![0, 0, 4, 1, 2, 4, 5, 10, 11];
-    // let mut input = vec![4, 1, 2, 4, 5, 10, 11];
-    // let input = &mut [6, 2, 3, 4, 5];
+    let mut input = vec![12, 4, 91, 2, 4, 5, 90, 67, 0, 15, 11, 10, 17];
 
+    println!("Input: \n{input:?}");
     let n = input.len() as i32;
-    quick(&mut input, 0, n - 1);
+    let target = n / 2;
+    sort_list_till_target(&mut input, 0, n - 1, target);
 
     println!("{input:?}");
+    println!("Output: {}", input[target as usize]);
 }
 
 fn _swap(input: &mut [i32], a: i32, b: i32) {
@@ -33,11 +33,28 @@ fn partition_hoares(input: &mut [i32], low: i32, high: i32) -> i32 {
         }
 
         if i >= j {
+            println!("{input:?} {j}");
             return j;
         }
 
         input.swap(i as usize, j as usize);
     }
+}
+
+fn partition_lomuto(input: &mut [i32], low: i32, high: i32) -> i32 {
+    let pivot = input[high as usize];
+    let mut i = low - 1;
+
+    for j in low..high {
+        if input[j as usize] <= pivot {
+            i += 1;
+            input.swap(i as usize, j as usize);
+        }
+    }
+    input.swap((i + 1) as usize, high as usize);
+    println!("{input:?} {i}");
+
+    i
 }
 
 fn partition(input: &mut [i32], low: i32, high: i32) -> i32 {
@@ -64,12 +81,18 @@ fn partition(input: &mut [i32], low: i32, high: i32) -> i32 {
     j
 }
 
-fn quick(input: &mut Vec<i32>, low: i32, high: i32) {
+fn sort_list_till_target(input: &mut Vec<i32>, low: i32, high: i32, target: i32) {
     if low < high {
-        let pi = partition_hoares(input, low, high);
-        quick(input, low, pi);
-        // in normal partition
-        // quick(input, low, pi - 1);
-        quick(input, pi + 1, high);
+        let pi = partition(input, low, high);
+        if pi == target {
+            return;
+        }
+
+        if pi > target {
+            sort_list_till_target(input, low, pi - 1, target);
+        }
+        if pi < target {
+            sort_list_till_target(input, pi + 1, high, target);
+        }
     }
 }
