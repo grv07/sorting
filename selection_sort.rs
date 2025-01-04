a3c6ade9535e24759ef2c9d3ffcb10b9a699f66f
Gaurav Tyagi <grvtyagi22@gmail.com>
2025-01-04 10:27:42 +0530

    Add recurssive solution too for selection sort

--- selection_sort.rs
+++ selection_sort.rs
@@ -15,10 +15,30 @@ fn sort<T: std::cmp::PartialOrd>(input: &mut [T]) {
     }
 }
 
+fn sort_rec<T: std::cmp::PartialOrd>(input: &mut [T], low: usize) {
+    if low >= input.len() {
+        return;
+    }
+
+    let mut smallest = low;
+    for j in low..input.len() {
+        if input[smallest] > input[j] {
+            smallest = j;
+        }
+    }
+
+    input.swap(low, smallest);
+
+    sort_rec(input, low + 1);
+}
+
 fn main() {
-    // let mut input: &mut [i32] = &mut [1, 4, 2, 3, 4];
-    let mut input: &mut [i32] = &mut [1, -4, 0, 890, 02, 3, 4];
+    let mut input: &mut [i32] = &mut [1, 4, 2, 3, 4];
+    let mut input1: &mut [i32] = &mut [1, -4, 0, 890, -2, 3, 4];
 
     sort(&mut input);
+    sort_rec(&mut input1, 0);
+
     println!("{input:?}");
+    println!("{input1:?}");
 }
