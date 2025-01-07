53a6cbc562ce04c0972c03dba8f4f6c60edc37dc
Gaurav Tyagi <grvtyagi22@gmail.com>
2025-01-07 19:24:57 +0530

    Solution done with readable code

--- arrays/longest_sub_array_sum_k.rs
+++ arrays/longest_sub_array_sum_k.rs
@@ -1,27 +1,20 @@
 fn solve(input: &[i32], t: i32) -> usize {
     let mut i = 0;
-    let mut j = 0;
+    // let mut j = 0;
 
     let mut sum = 0;
     let mut maxi = 0;
 
-    while i <= j && j < input.len() {
-        if sum < t {
-            sum += input[j];
-            j += 1;
-        }
-
-        if sum > t {
+    for j in 0..input.len() {
+        while sum > t && i <= j {
             sum -= input[i];
             i += 1;
         }
+
+        sum += input[j];
 
         if sum == t {
-            // println!("sum found at {i} {j}");
-            maxi = std::cmp::max(maxi, j - i);
-
-            sum -= input[i];
-            i += 1;
+            maxi = std::cmp::max(maxi, j - i + 1);
         }
     }
 
@@ -29,12 +22,26 @@ fn solve(input: &[i32], t: i32) -> usize {
 }
 
 fn main() {
-    let res = solve(&[1, 1, 1, 1, 2, 2, 4, 2], 4);
+    let res = solve(&[1, 1, 1, 1, 2, 2, 4, 0, 0, 0, 0, 0, 2, 2], 4);
     println!("{res}");
 
-    let res = solve(&[4, 2, 1, 1, 2, 2, 1, 1, 1, 1], 3);
+    let res = solve(
+        &[
+            4, 2, 1, 1, 2, 2, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 13, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 3, 1,
+        ],
+        3,
+    );
     println!("{res}");
 
-    // let res = solve(&[1, 2, 3, 1, 1, 1, 1, 2, 2, 4, 2, 3, 0, 0, 0, 0, 2], 2);
-    // println!("{res}");
+    let res = solve(
+        &[
+            4, 2, 1, 1, 2, 2, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0,
+        ],
+        3,
+    );
+    println!("{res}");
+
+    let res = solve(&[1, 2, 3, 1, 1, 1, 1, 2, 2, 4, 2, 3, 0, 0, 0, 0, 2], 2);
+    println!("{res}");
 }
