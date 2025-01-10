066094ee6ae9530f6b1b809c5ee78c0647db12aa
Gaurav Tyagi <grvtyagi22@gmail.com>
2025-01-10 21:04:08 +0530

    Rearrange solution for minus and plus

--- arrays/arrange_by_sign.rs
+++ arrays/arrange_by_sign.rs
@@ -1,3 +1,26 @@
+fn solve_easy<T: std::cmp::PartialOrd + Copy + From<i32>>(a: &mut [T]) -> Vec<T> {
+    let n = a.len();
+    let mut e = 0;
+    let mut o = 1;
+
+    let mut res: Vec<T> = vec![0.into(); n];
+
+    for i in 0..n {
+        // println!("{e} {o} {}", a[i]);
+
+        if a[i] >= 0.into() {
+            res[e] = a[i];
+            e += 2;
+        }
+
+        if a[i] < 0.into() && o < n {
+            res[o] = a[i];
+            o += 2;
+        }
+    }
+    res
+}
+
 fn solve(a: &mut [i32]) {
     let n = a.len() as i32;
     let mut left = 0 as i32;
@@ -29,4 +52,11 @@ fn main() {
     println!("{a:?}");
     solve(a);
     println!("{a:?}");
+
+    println!();
+
+    let a = &mut [-1, 3, -2, 1, 2, -3, -4, -6, 4, 5, 6, -5, -7, -8, 7, 8];
+    println!("{a:?}");
+    let res = solve_easy(a);
+    println!("{res:?}");
 }
