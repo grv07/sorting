6a6dffa039807c6761becc2bc9dc154f096e257d
Gaurav Tyagi <grvtyagi22@gmail.com>
2025-01-27 12:57:50 +0530

    merge interval brute force

---
+++ arrays/interval_merge.rs
@@ -0,0 +1,58 @@
+type Range = (i32, i32);
+
+fn merge(a: Range, b: Range) -> Option<Range> {
+    let (x1, y1) = a;
+    let (x2, y2) = b;
+
+    // Check if lines are not overlapping
+    if y1 < x2 || y2 < x1 {
+        return None;
+    }
+
+    return Some((x1.min(x2), y1.max(y2)));
+}
+
+fn bf(a: &[Range]) -> Vec<Range> {
+    let mut res = vec![];
+    let n = a.len();
+
+    let mut i = 0;
+    let mut j = 0;
+    let mut temp = a[i];
+    while i <= j && j < n {
+        temp = if let Some(r) = merge(temp, a[j]) {
+            r
+        } else {
+            res.push(temp);
+
+            i = j;
+            a[j]
+        };
+
+        j += 1;
+    }
+    res.push(temp);
+
+    res
+}
+
+fn solve() {
+    let a = &[
+        (1, 3),
+        (2, 6),
+        (8, 9),
+        (9, 11),
+        (8, 10),
+        (2, 4),
+        (15, 18),
+        (16, 17),
+    ];
+
+    let res = bf(a);
+    println!("{a:?}");
+    println!("{res:?}");
+}
+
+fn main() {
+    solve();
+}
