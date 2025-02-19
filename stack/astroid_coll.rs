3dd37406a010a3d7a2cafdc1372e4a37f309bfc9
Gaurav Tyagi <grvtyagi22@gmail.com>
2025-02-19 19:14:08 +0530

    Astroid collisions done

---
+++ stack/astroid_coll.rs
@@ -0,0 +1,63 @@
+#[derive(std::cmp::PartialEq)]
+enum Move {
+    Left,
+    Right,
+}
+
+fn get_move(i: i32) -> Move {
+    if i < 0 {
+        Move::Left
+    } else {
+        Move::Right
+    }
+}
+
+fn solve(a: &[i32]) -> Vec<i32> {
+    let mut s = vec![];
+
+    for i in 0..a.len() {
+        let m = get_move(a[i]);
+        if s.is_empty() {
+            s.push(a[i]);
+            continue;
+        }
+
+        if let Some(last) = s.last() {
+            if m == get_move(*last) {
+                s.push(a[i]);
+                continue;
+            }
+        }
+
+        while let Some(l) = s.last() {
+            if m != get_move(*l) {
+                let current = a[i].abs();
+                let mut last = l.abs();
+                // println!(" >> {} {} {s:?}", a[i], l);
+
+                if last == current {
+                    s.pop();
+                    break;
+                }
+
+                if !s.is_empty() && last < current {
+                    s.pop();
+                    if s.is_empty() {
+                        s.push(a[i]);
+                        break;
+                    }
+                    continue;
+                }
+            }
+
+            break;
+        }
+    }
+
+    s
+}
+
+fn main() {
+    let res = solve(&[1, 7, 2, 3, -7, -9, 10]);
+    println!("{res:?}");
+}
