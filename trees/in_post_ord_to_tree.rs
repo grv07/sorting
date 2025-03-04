81893424d93969334003086ac4e9271fec909a56
Gaurav Tyagi <grvtyagi22@gmail.com>
2025-03-04 20:28:54 +0530

    CLean the code

--- trees/in_post_ord_to_tree.rs
+++ trees/in_post_ord_to_tree.rs
@@ -18,8 +18,8 @@ fn create_tree(in_o: &[i32], post_o: &[i32]) -> Option<Box<Node<i32>>> {
         let r_pos_o = &post_o[i..n - 1];
         let l_pos_o = &post_o[0..i];
 
-        println!("left {l_in_o:?} right {r_in_o:?}");
-        println!("left {l_pos_o:?} right {r_pos_o:?}");
+        // println!("left {l_in_o:?} right {r_in_o:?}");
+        // println!("left {l_pos_o:?} right {r_pos_o:?}");
 
         node.left = create_tree(l_in_o, l_pos_o);
         node.right = create_tree(r_in_o, r_pos_o);
@@ -32,8 +32,6 @@ pub fn solve() -> Node<i32> {
     let in_o = &[40, 20, 50, 10, 60, 30];
     let post_o = &[40, 50, 20, 60, 30, 10];
     let tree = create_tree(in_o, post_o);
-    println!("Tree: {tree:?}");
+    // println!("Tree: {tree:?}");
     *tree.unwrap()
 }
-
-// fn main() {}
