890ce494daaaff138225b4d879eff4750fd89c86
Gaurav Tyagi <grvtyagi22@gmail.com>
2025-04-06 12:57:02 +0530

    More optiomize solution in term of leetcode

--- two_pointer/abc_sub_string.rs
+++ two_pointer/abc_sub_string.rs
@@ -20,8 +20,36 @@ fn solve(s: &str) -> usize {
     sum
 }
 
+fn solve2(s: &str) -> usize {
+    let s = s.as_bytes();
+    let mut p = [0; 3];
+    let mut l = 0;
+    let mut r = 0;
+    let mut cnt = 0;
+
+    'outer: while r < s.len() - 2 {
+        while p[0] == 0 || p[1] == 0 || p[2] == 0 {
+            if r >= s.len() {
+                break 'outer;
+            }
+
+            p[s[r] as usize - 'a' as usize] += 1;
+            r += 1;
+        }
+
+        cnt += s.len() - r + 1;
+
+        p[s[l] as usize - 'a' as usize] -= 1;
+
+        l += 1;
+    }
+
+    cnt
+}
+
 fn main() {
     assert_eq!(solve("abcbbbb"), 5);
+    assert_eq!(solve2("abcbbbb"), 5);
     assert_eq!(solve("aabcbbbb"), 10);
     assert_eq!(solve("abcabc"), 10);
     assert_eq!(solve("aaacb"), 3);
