94962fc134a414abd7987df2a9c2796778595d9f
Gaurav Tyagi <grvtyagi22@gmail.com>
2025-04-14 19:51:37 +0530

    UPdate the algo's for increasing  and insert

--- heap/heap.rs
+++ heap/heap.rs
@@ -10,6 +10,7 @@ impl<T: Copy + std::cmp::PartialOrd> Heap<T> {
         Heap { a, size }
     }
 
+    #[allow(dead_code)]
     fn build_heap(&mut self) {
         let mut i = self.size as i32 / 2;
         while i >= 0 {
@@ -66,8 +67,42 @@ impl<T: Copy + std::cmp::PartialOrd + std::ops::AddAssign<i32>> Heap<T> {
         Some(top)
     }
 
-    fn update_key(&mut self, i: usize, delta: i32) {
-        self.a[i] += delta;
+    fn insert(&mut self, delta: T) {
+        self.a.push(delta);
+        self.size += 1;
+
+        // println!("Size: {}", self.size);
+        self.increase_key(self.size - 1, delta);
+    }
+
+    fn increase_key(&mut self, i: usize, delta: T) {
+        let a = &mut self.a;
+
+        if self.size < i || a[i] > delta {
+            return;
+        }
+
+        a[i] = delta;
+
+        let mut i = i;
+
+        while i > 0 {
+            let p = i / 2;
+            if a[p] < a[i] {
+                a.swap(i, p);
+                i = p;
+            } else {
+                break;
+            }
+        }
+    }
+
+    fn decrease_key(&mut self, i: usize, delta: T) {
+        if self.size < i || self.a[i] < delta {
+            return;
+        }
+        self.a[i] = delta;
+
         self.build_heap();
     }
 }
@@ -76,19 +111,44 @@ fn main() {
     let mut heap = Heap::new(vec![7, 5, 6, 3, 4, 1, 2]);
 
     let item = heap.pop();
-    println!("Res: {item:?}");
+    println!("POP: {item:?}");
 
     let item = heap.pop();
-    println!("Res: {item:?}");
+    println!("POP: {item:?}");
 
     let item = heap.pop();
-    println!("Res: {item:?}");
+    println!("POP: {item:?}");
 
     println!("Heap: {heap:?}");
 
-    heap.update_key(3, 45);
-    println!("Heap: {heap:?}");
+    heap.increase_key(3, 45);
+    println!("INC_K 3: {heap:?}");
 
-    let item = heap.pop();
-    println!("Res: {item:?}");
+    heap.increase_key(2, 46);
+    println!("INC_K 2: {heap:?}");
+
+    heap.decrease_key(0, 45);
+    println!("INC_K 0: {heap:?}");
+
+    heap.decrease_key(3, 1);
+    println!("DEC_K 0: {heap:?}");
+
+    heap.decrease_key(2, 3);
+    println!("DEC_K 2: {heap:?}");
+
+    heap.insert(23);
+    println!("INSERT 23: {heap:?}");
+
+    heap.insert(1);
+    println!("INSERT 1: {heap:?}");
+
+    heap.insert(25);
+    println!("INSERT 25: {heap:?}");
+
+    heap.increase_key(6, 6);
+    println!("INC_K 6: {heap:?}");
+
+    while let Some(item) = heap.pop() {
+        println!("POP: {item:?}");
+    }
 }
